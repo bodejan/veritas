4e72479eb260661fe88e80d43934146840c7c088
FuratH <hamdanfurat@gmail.com>
2023-05-15 20:08:03 +0200

    Add statistic overview

--- frontend/src/pages/CheckCategory.tsx
+++ frontend/src/pages/CheckCategory.tsx
@@ -16,20 +16,74 @@ export default function CheckCategory(props: CategoryProps): ReactElement<Catego
       numApps: 1,
     },
 
-    /*
-    validate: {
-      category: (value) => (value ? null : 'Invalid category'),
-      numApps: (value) => (value < 2 ? null : 'Invalid number of apps'),
-    },
-*/
   });
 
   const { errors, getInputProps } = form;
 
+
+  const categories = JSON.parse(JSON.stringify({
+    "All": "",
+    "Paid": "?price=paid",
+    "Free": "?price=free",
+    "Art And Design": "?category=ART_AND_DESIGN",
+    "Auto And Vehicles": "?category=AUTO_AND_VEHICLES",
+    "Beauty": "?category=BEAUTY",
+    "Books And Reference": "?category=BOOKS_AND_REFERENCE",
+    "Business": "?category=BUSINESS",
+    "Comics": "?category=COMICS",
+    "Communication": "?category=COMMUNICATION",
+    "Dating": "?category=DATING",
+    "Education": "?category=EDUCATION",
+    "Entertainment": "?category=ENTERTAINMENT",
+    "Events": "?category=EVENTS",
+    "Finance": "?category=FINANCE",
+    "Food And Drink": "?category=FOOD_AND_DRINK",
+    "Health And Fitness": "?category=HEALTH_AND_FITNESS",
+    "House And Home": "?category=HOUSE_AND_HOME",
+    "Libraries And Demo": "?category=LIBRARIES_AND_DEMO",
+    "Lifestyle": "?category=LIFESTYLE",
+    "Maps And Navigation": "?category=MAPS_AND_NAVIGATION",
+    "Medical": "?category=MEDICAL",
+    "Music And Audio": "?category=MUSIC_AND_AUDIO",
+    "News And Magazines": "?category=NEWS_AND_MAGAZINES",
+    "Parenting": "?category=PARENTING",
+    "Personalization": "?category=PERSONALIZATION",
+    "Photography": "?category=PHOTOGRAPHY",
+    "Productivity": "?category=PRODUCTIVITY",
+    "Shopping": "?category=SHOPPING",
+    "Social": "?category=SOCIAL",
+    "Sports": "?category=SPORTS",
+    "Tools": "?category=TOOLS",
+    "Transportation": "?category=TRANSPORTATION",
+    "Travel And Local": "?category=TRAVEL_AND_LOCAL",
+    "Video Players": "?category=VIDEO_PLAYERS",
+    "Weather": "?category=WEATHER",
+    "Game Action": "?category=GAME_ACTION",
+    "Game Adventure": "?category=GAME_ADVENTURE",
+    "Game Arcade": "?category=GAME_ARCADE",
+    "Game Board": "?category=GAME_BOARD",
+    "Game Card": "?category=GAME_CARD",
+    "Game Casino": "?category=GAME_CASINO",
+    "Game Casual": "?category=GAME_CASUAL",
+    "Game Educational": "?category=GAME_EDUCATIONAL",
+    "Game Family": "?category=GAME_FAMILY",
+    "Game Music": "?category=GAME_MUSIC",
+    "Game Puzzle": "?category=GAME_PUZZLE",
+    "Game Racing": "?category=GAME_RACING",
+    "Game Role Playing": "?category=GAME_ROLE_PLAYING",
+    "Game Simulation": "?category=GAME_SIMULATION",
+    "Game Sports": "?category=GAME_SPORTS",
+    "Game Strategy": "?category=GAME_STRATEGY",
+    "Game Trivia": "?category=GAME_TRIVIA",
+    "Game Word": "?category=GAME_WORD"
+}))
+
   const handleSubmit = () => {
     if (form.values.category && form.values.numApps >= 1) {
-      console.log('Selected category:', form.values.category);
-      console.log('Number of apps:', form.values.numApps);
+
+      var categoryObject = {category : form.values.category, number: form.values.numApps}
+
+      console.log(categoryObject)
     }
   };
 
@@ -46,7 +100,7 @@ export default function CheckCategory(props: CategoryProps): ReactElement<Catego
             <Grid.Col xs={12} lg={6}>
               <NativeSelect
                 {...getInputProps('category')}
-                data={['Category1', 'Category2', 'Category3', 'Category4']}
+                data={Object.keys(categories)}
                 label="Select category"
                 description="Select a category for apps you want to check"
                 withAsterisk
--- frontend/src/pages/Statistics/Overview.tsx
+++ frontend/src/pages/Statistics/Overview.tsx
@@ -1,7 +1,209 @@
-import React from 'react'
+import { Avatar, Box, Button, Flex, Grid, Image, Progress, RingProgress, ScrollArea, Stack, Text, Title, createStyles} from '@mantine/core'
+import React, { useState } from 'react'
+
+const useStyles = createStyles((theme) => ({
+    scollbox: {
+        maxHeight: 300,
+       
+        padding: 20,
+        background: theme.colors.gray[1],
+        borderRadius: 8,
+    }
+  }));
 
 export default function Overview() {
+    const { classes, theme } = useStyles();
+
+
+
+    const [appdata, setAppdata] = useState(JSON.parse(JSON.stringify([
+        {
+          "id": "com.google.android.youtube",
+          "name": "YouTube",
+          "image": "https://play-lh.googleusercontent.com/lMoItBgdPPVDJsNOVtP26EKHePkwBg-PkuY9NOrc-fumRtTFP4XhpUNk_22syN4Datc=s96-rw",
+          "policies": {
+            "Data Categories": 0,
+            "Processing Purpose": 1,
+            "Data Recipients": 0,
+            "Source of Data": 1,
+            "Provision Requirement": 1,
+            "Data Safeguards": 1,
+            "Profiling": 1,
+            "Storage Period": 0,
+            "Adequacy Decision": 0,
+            "Controllers Contact": 1,
+            "DPO Contact": 1,
+            "Withdraw Consent": 1,
+            "Lodge Complaint": 1,
+            "Right to Access": 1,
+            "Right to Erase": 1,
+            "Right to Restrict": 0,
+            "Right to Object": 1,
+            "Right to Data Portability": 0
+          }
+        },
+        {
+          "id": "facebook",
+          "name": "Facebook",
+          "image": "https://play.google.com/store/apps/details?id=com.facebook.katana",
+          "policies": {
+            "Data Categories": 0,
+            "Processing Purpose": 1,
+            "Data Recipients": 0,
+            "Source of Data": 1,
+            "Provision Requirement": 1,
+            "Data Safeguards": 1,
+            "Profiling": 1,
+            "Storage Period": 0,
+            "Adequacy Decision": 0,
+            "Controllers Contact": 1,
+            "DPO Contact": 1,
+            "Withdraw Consent": 0,
+            "Lodge Complaint": 0,
+            "Right to Access": 0,
+            "Right to Erase": 1,
+            "Right to Restrict": 0,
+            "Right to Object": 1,
+            "Right to Data Portability": 0
+          }
+        }
+      ])))
+
+      interface Policy {
+        [key: string]: number;
+      }
+      
+      interface PolicyObject {
+        id: string;
+        name: string;
+        image: string;
+        policies: Policy;
+      }
+      
+      function combinePolicies(arr: PolicyObject[]): Policy {
+        const combinedPolicies: Policy = {};
+      
+        for (let i = 0; i < arr.length; i++) {
+          const policies = arr[i].policies;
+          const policyKeys = Object.keys(policies);
+      
+          for (let j = 0; j < policyKeys.length; j++) {
+            const key = policyKeys[j];
+            const value = policies[key];
+      
+            if (combinedPolicies.hasOwnProperty(key)) {
+              combinedPolicies[key] += value;
+            } else {
+              combinedPolicies[key] = value;
+            }
+          }
+        }
+      
+        return combinedPolicies;
+      }
+
+      function calculateSumOfPolicies(policies: Policy) {
+        let sum = 0;
+        for (const key in policies) {
+          if (typeof policies[key] === "number") {
+            sum += policies[key];
+          }
+        }
+        return sum;
+      }
+
   return (
-    <div>Overview</div>
+    <>
+      <Stack p={20}>
+        <section>
+          <Title>Check privacy policies by app category</Title>
+          <Text>Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. </Text>
+        </section>
+
+        <Grid>
+            <Grid.Col span={9}>
+
+            </Grid.Col>
+            <Grid.Col span={3} sx={{justifyContent: "end", display:"flex"}}>
+                <Button color="dark">
+                    Export data
+                </Button>
+            </Grid.Col>
+        </Grid>
+       
+
+        <section>
+          <Grid>
+            <Grid.Col xs={12} lg={3}>
+            <RingProgress
+                sections={[{ value: 40, color: "teal" }]}
+                size={280}
+                thickness={17}
+                roundCaps
+                label={
+                <Text color="teal" weight={700} align="center" size="40px">
+                    12 / 18
+                </Text>
+                }
+            />
+            </Grid.Col>
+            <Grid.Col xs={12} lg={9}>
+                <ScrollArea className={classes.scollbox}>
+                    <Grid p={10}>
+                    {Object.keys(combinePolicies(appdata)).map(value => 
+                    <>
+                    <Grid.Col span={3}>
+                            <Title order={6}>{value}</Title>
+                        </Grid.Col>
+                        <Grid.Col span={7} display="grid" sx={{alignContent: "center"}}>
+                            <Progress value={(combinePolicies(appdata)[value] / appdata.length) * 100} size="xl" color={theme.colors.yellow[5]}/>
+                        </Grid.Col>
+                        <Grid.Col span={2}>
+                        <Title order={6}>{combinePolicies(appdata)[value]} / {appdata.length} Apps</Title>
+                        </Grid.Col>
+                    
+                    </>    
+                    )        
+                    }
+                   
+                    </Grid>
+                </ScrollArea>
+
+            </Grid.Col>
+
+          </Grid>
+        </section>
+
+        <section>
+            <ScrollArea className={classes.scollbox}>
+                
+                {appdata.map((value : PolicyObject) => 
+                    <Grid mb={20} p={10} sx={{borderRadius: 20}} bg={theme.colors.gray[0]}>
+                        <Grid.Col span={2}>
+                        <Avatar src={value.image} />
+                           
+                        </Grid.Col>
+                        <Grid.Col span={2} >
+                            <Title order={6}>{value.name}</Title>
+                        </Grid.Col>
+                        <Grid.Col span={4} display="grid" sx={{alignContent: "center"}}>
+                                <Progress value={Object.keys(value.policies).length} size="xl" color={theme.colors.gray[5]}/>
+                        </Grid.Col>
+                        <Grid.Col span={2}>
+                            <Title order={6}>{calculateSumOfPolicies(value.policies)} / {Object.keys(value.policies).length}</Title>
+                        </Grid.Col>
+                        <Grid.Col span={2}>
+                            <Button color="dark" variant='outline'>More Info</Button>
+                        </Grid.Col>
+                    
+                    </Grid>  
+                )        
+                }
+                
+                
+            </ScrollArea>
+        </section>
+      </Stack>
+    </>
   )
 }
