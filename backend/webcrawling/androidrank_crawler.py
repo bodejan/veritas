a8ffa757f55a68f769ae251664913775f81fc4e7
bod3 <78141647+bodejan@users.noreply.github.com>
2023-06-13 13:11:44 +0200

    fix click_next_page bug

--- backend/webcrawling/androidrank_crawler.py
+++ backend/webcrawling/androidrank_crawler.py
@@ -5,13 +5,16 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.firefox.options import Options
 from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+
 from models import CATEGORIES
 
 categories = CATEGORIES
 
 
 def get_ids_for_category(category, number):
-    print('\n', f'Getting top {number} ids for {category}')
+    print(f'Getting top {number} ids for {category}')
     ids = []
     url = "https://androidrank.org/android-most-popular-google-play-apps"
     driver = None
@@ -20,7 +23,9 @@ def get_ids_for_category(category, number):
         driver = webdriver.Remote('http://chrome:4444/wd/hub',options=webdriver.ChromeOptions())
         driver.get(f'{url}{categories[category]}')
 
+
         while (len(ids) < number):
+            time.sleep(3)
             links = driver.find_element(By.TAG_NAME, 'tbody').find_elements(By.TAG_NAME, 'a')
             links = list(map(lambda x: x.get_attribute("href"), links))
             # match id of apps embedded in links and add to apps
@@ -33,7 +38,6 @@ def get_ids_for_category(category, number):
             if len(ids) == number: break
             driver = click_next_page(driver)
             if driver == None: break
-            time.sleep(4)
         
     except Exception as e:
         print(f'Error while crawling top {number} from {category}')
@@ -73,20 +77,31 @@ def get_ids_on_page(ids, driver, number):
 def click_next_page(driver):
     # does not work for some reason
     # next_btn = driver.find_element(By.XPATH, "//a[@name='Next >']")
+    # neither does
+    # next_btn = driver.find_element(By.XPATH, '//*[@id="content"]/small/a[1||3]')    
     try:
         # Last page starts has 'start=481' - no need to click next page
         if 'start=481' not in driver.current_url:
             # "First" and "Previous" buttons are disabled on the first page resulting in a different XPATH
             if 'start=' in driver.current_url:
+                wait = WebDriverWait(driver, 10)  # Maximum wait time of 10 seconds
+                next_btn = wait.until(EC.element_to_be_clickable((By.XPATH, "/html/body/div[1]/div[2]/div[1]/small/a[3]")))
                 next_btn = driver.find_element(By.XPATH, "/html/body/div[1]/div[2]/div[1]/small/a[3]")
             else:
-                next_btn = driver.find_element(By.XPATH, "/html/body/div[1]/div[2]/div[1]/small/a[1]")
+                # Wait until the button becomes clickable
+                wait = WebDriverWait(driver, 10)  # Maximum wait time of 10 seconds
+                next_btn = wait.until(EC.element_to_be_clickable((By.XPATH, "/html/body/div[1]/div[2]/div[1]/small/a[1]")))
             
-            time.sleep(1)
-            next_btn.click()
+            # Get the href attribute value
+            href = next_btn.get_attribute('href')
+
+            # Open next page
+            driver.get(href)
+
     except Exception as e:
         print(f'Cannot click "next"')
         print(f'Current url: {driver.current_url}')
+        print(e)
         if driver is not None:
             driver.quit()
         return None
